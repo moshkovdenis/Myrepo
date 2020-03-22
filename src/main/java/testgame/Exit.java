ad7229935799f05d44fe600a4b69b14d8fe4aa94
moshkovdenis <moshkovdenis@mail.ru>
2020-03-22 13:48:39 +0300

    class Exit

---
+++ src/main/java/testgame/Exit.java
@@ -0,0 +1,8 @@
+package testgame;
+
+public class Exit {
+    public static void exitGames() {
+        System.out.println("Спасибо за игру! Буду ждать тебя снова:)");
+        System.exit(0);
+    }
+}
