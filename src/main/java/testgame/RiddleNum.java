dbeba5bedf8fcebde69d3cbfd0ecd29be8930ac7
moshkovdenis <moshkovdenis@mail.ru>
2020-03-24 19:41:28 +0300

    some errors

--- src/main/java/testgame/RiddleNum.java
+++ src/main/java/testgame/RiddleNum.java
@@ -4,18 +4,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RiddleNum {
-    private static   Scanner sc = new Scanner(System.in);
-    private static   Random rnd = new Random();
     private static int min = 0;
     private static int max = 100;
-    private static int resNum = rnd.nextInt(max - min) + min;
-
     public static boolean programGuess() {
-        System.out.println("Для выхода введите \"выйти\"");
-        System.out.println(resNum);
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random();
+        int resNum = rnd.nextInt(max - min) + min;
         while (true) {
             try {
+                System.out.println("Для выхода введите \"выйти\"");
                 System.out.println("если ваше число больше введите \"больше\", если меньше введите \"меньше");
+                System.out.println("если число верно введи \"правильно\".");
+                System.out.println(resNum);
                 String input = sc.nextLine();
                 if(input.equalsIgnoreCase("выйти")){
                     return true;
@@ -23,17 +23,15 @@ public class RiddleNum {
                 if (input.equalsIgnoreCase("меньше")) {
                     max = resNum;
                     resNum = rnd.nextInt(max - min) + min;
-                    System.out.println(resNum);
                 } else if (input.equalsIgnoreCase("больше")) {
                     min = resNum + 1;
                     resNum = rnd.nextInt(max - min) + min;
-                    System.out.println(resNum);
                 } else if (input.equalsIgnoreCase("правильно")) {
                     System.out.println("Система победила! Спасибо за игру человек.");
                     return true;
                 }
             } catch (IllegalArgumentException e) {
-                System.out.println("Обманывать плохо,ваще число " + resNum);
+                System.out.println("Обманывать плохо, твоё число " + resNum);
                 return true;
             }
 
